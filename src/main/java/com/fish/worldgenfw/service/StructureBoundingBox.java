8ba27d76c9cdef87e6f5a669f1e90b212c91854d
filashi <497437901@qq.com>
2026-05-23 20:21:44 +0800

    二次重构

--- src/main/java/com/fish/worldgenfw/service/StructureBoundingBox.java
+++
@@ -1,7 +0,0 @@
-// com/fish/worldgenfw/service/StructureBoundingBox.java
-package com.fish.worldgenfw.service;
-
-import net.minecraft.world.phys.AABB;
-
-public record StructureBoundingBox(AABB box, String structureId, int instanceId) {
-}
--- src/main/java/com/fish/worldgenfw/util/InstanceIdContext.java
+++
@@ -1,9 +0,0 @@
-package com.fish.worldgenfw.util;
-
-public class InstanceIdContext {
-    private static final ThreadLocal<Integer> NEXT_INSTANCE_ID = new ThreadLocal<>();
-
-    public static void setNextId(int id) { NEXT_INSTANCE_ID.set(id); }
-    public static Integer getNextId() { return NEXT_INSTANCE_ID.get(); }
-    public static void clear() { NEXT_INSTANCE_ID.remove(); }
-}
